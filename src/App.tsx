8e1fb0c22a7275be38bfffebc2c8564fec149d3b
Athilla <luthfi.thilla@gmail.com>
2021-04-02 13:41:42 +0700

    Basic convert and setup to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Tool from './container/Tool';
 
 const App = () => {
   return (
-    <div className="container w-4/5 mx-auto py-20 flex flex-colcontainer w-4/5 mx-auto py-20 flex flex-col">
+    <div className="container w-4/5 mx-auto py-20 flex flex-col">
       <Intro />
       <Tool />
     </div>
