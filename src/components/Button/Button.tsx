798042846c4818db591aa23adc80ac555cf6ce25
Athilla <luthfi.thilla@gmail.com>
2021-04-02 16:04:09 +0700

    Converted to typescript

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
-const Button = (props) => {
+import { MouseEventHandler } from 'react';
+
+interface buttonProps {
+  type: 'button' | 'submit' | 'reset';
+  onclick: MouseEventHandler;
+  text: string;
+}
+
+const Button = ({ type, onclick, text }: buttonProps) => {
   return (
     <button
-      type={props.type}
+      type={type}
       className="w-1/5 bg-green-500 text-white font-medium px-5 py-3 border-2 border-green-600 rounded-full ml-10"
-      onClick={props.onclick}
+      onClick={onclick}
     >
-      {props.text}
+      {text}
     </button>
   );
 };
--- src/container/Tools/Panel.tsx
+++ src/container/Tools/Panel.tsx
@@ -1,16 +1,22 @@
+import { FormEvent, MouseEventHandler } from 'react';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 
-const Panel = (props) => {
-  const generateHandler = (event) => {
+interface panelProps {
+  value: string;
+  onclick: MouseEventHandler;
+}
+
+const Panel = ({ value, onclick }: panelProps) => {
+  const generateHandler = (event: FormEvent) => {
     event.preventDefault();
   };
 
   return (
     <div>
       <form onSubmit={generateHandler} className="flex flex-row">
-        <Input type="text" color="green" value={props.value} />
-        <Button type="submit" text="Generate" onclick={props.onclick} />
+        <Input type="text" color="green" text={value} />
+        <Button type="button" text="Generate" onclick={onclick} />
       </form>
     </div>
   );
